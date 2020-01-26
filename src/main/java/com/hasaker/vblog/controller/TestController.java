d3c0e55c711b1759dfb9f0f60b197d09c0370b8a
Hasaker <1758267482@qq.com>
2020-01-26 20:48:28 +0800

    添加Swagger

--- src/main/java/com/hasaker/vblog/controller/TestController.java
+++ src/main/java/com/hasaker/vblog/controller/TestController.java
@@ -2,6 +2,8 @@ package com.hasaker.vblog.controller;
 
 import com.hasaker.vblog.common.AjaxResult;
 import com.hasaker.vblog.exception.enums.CommonExceptionEnums;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @create 2020/1/6 15:27
  * @description TestController
  */
+@Api(value = "测试Controller")
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
     @GetMapping("/exception")
+    @ApiOperation(value = "测试异常")
     public AjaxResult testException() {
         throw CommonExceptionEnums.NOT_NULL_ARG.newException("测试Exception!");
 
