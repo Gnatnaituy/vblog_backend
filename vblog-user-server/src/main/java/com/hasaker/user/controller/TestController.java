87a94845ea2fb7af81ac9f674173c31a61bef58c
Hasaker <1758267482@qq.com>
2020-02-09 17:12:42 +0800

    consul config配置成功

---
+++ vblog-user-server/src/main/java/com/hasaker/user/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.hasaker.user.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @package com.hasaker.user.controller
+ * @author 余天堂
+ * @create 2020/2/9 16:25
+ * @description TestController
+ */
+@RestController(value = "/user")
+public class TestController {
+
+}
