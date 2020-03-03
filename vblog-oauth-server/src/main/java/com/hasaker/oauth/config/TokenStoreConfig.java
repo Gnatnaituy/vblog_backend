cf9e69e9d369091b205d58d7e5428346708ccdd9
Hasaker <1758267482@qq.com>
2020-03-03 21:44:50 +0800

    修改redis连接方式

--- vblog-oauth-server/src/main/java/com/hasaker/oauth/config/TokenStoreConfig.java
+++ vblog-oauth-server/src/main/java/com/hasaker/oauth/config/TokenStoreConfig.java
@@ -1,14 +1,17 @@
 package com.hasaker.oauth.config;
 
 import com.hasaker.common.consts.Consts;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 /**
  * @package com.hasaker.oauth.config
@@ -19,6 +22,9 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 public class TokenStoreConfig {
 
+    /**
+     * JWT储存token
+     */
     @Configuration
     @ConditionalOnProperty(prefix = "oauth2.token.store", name = "type", havingValue = "jwt", matchIfMissing = true)
     public static class JwtTokenStoreConfig {
@@ -40,6 +46,22 @@ public class TokenStoreConfig {
         @Bean
         public TokenEnhancer jwtTokenEnhancer(){
             return new JwtTokenEnhancer();
+        }
+    }
+
+    /**
+     * Redis储存token
+     */
+    @Configuration
+    @ConditionalOnProperty(prefix = "oauth2.token.store", name = "type", havingValue = "jwt")
+    public static class RedisTokenStoreConfig {
+
+        @Autowired(required = false)
+        private RedisConnectionFactory redisConnectionFactory;
+
+        @Bean
+        public TokenStore redisTokenStore() {
+            return new RedisTokenStore(redisConnectionFactory);
         }
     }
 }
