1904977991603be47a564f5a4ba9af0e9c93a5fd
Hasaker <1758267482@qq.com>
2020-04-09 00:24:42 +0800

    Qiniu upload oss

---
+++ vblog-face-server/src/main/java/com/hasaker/face/controller/base/UploadController.java
@@ -0,0 +1,29 @@
+package com.hasaker.face.controller.base;
+
+import com.hasaker.common.vo.Ajax;
+import com.hasaker.component.qiniu.service.UploadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @package com.hasaker.face.controller.base
+ * @author 余天堂
+ * @create 2020/4/8 19:18
+ * @description UploadController
+ */
+@RestController
+@RequestMapping("/upload")
+public class UploadController {
+
+    @Autowired
+    private UploadService uploadService;
+
+    @PostMapping("/image")
+    Ajax<String> upload(@RequestParam("file") MultipartFile multipartFile) {
+        return uploadService.upload(multipartFile);
+    }
+}
